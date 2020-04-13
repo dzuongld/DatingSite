64a5ca7a8a0fe276475eaa2d23e177d6fe577cdb
Duong Lam <dzuongld@gmail.com>
2020-04-13 19:56:45 +1000

    Frontend 3rd party packages
    - alertify
    - jwt helper
    - ngx bootstrap

--- DatingSite-SPA/src/app/nav/nav.component.ts
+++ DatingSite-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { AuthService } from '../_services/auth.service'
+import { AlertifyService } from '../_services/alertify.service'
 
 @Component({
     selector: 'app-nav',
@@ -9,7 +10,7 @@ import { AuthService } from '../_services/auth.service'
 export class NavComponent implements OnInit {
     model: any = {}
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private alertify: AlertifyService) {}
 
     ngOnInit(): void {}
 
@@ -17,21 +18,24 @@ export class NavComponent implements OnInit {
         // login return observable => must subscribe
         this.authService.login(this.model).subscribe(
             (next) => {
-                console.log('Logged in')
+                this.alertify.success('Logged in')
             },
             (error) => {
-                console.log(error)
+                this.alertify.error(error)
             }
         )
     }
 
     loggedIn() {
-        const token = localStorage.getItem('token')
-        return !!token
+        return this.authService.loggedIn()
     }
 
     logout() {
         localStorage.removeItem('token')
-        console.log('Logged out')
+        this.alertify.message('Logged out')
+    }
+
+    getName() {
+        return this.authService.decodedToken?.unique_name
     }
 }
