e854439ecb6b9ae8ab62b5f52007ebf405cce4df
Duong Lam <dzuongld@gmail.com>
2020-04-16 17:25:24 +1000

    Frontend routing with guard

--- DatingSite-SPA/src/app/app.module.ts
+++ DatingSite-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule, OnInit } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
 
@@ -11,10 +12,29 @@ import { AuthService } from './_services/auth.service'
 import { HomeComponent } from './home/home.component'
 import { RegisterComponent } from './register/register.component'
 import { ErrorInterCeptorProvider } from './_services/error.interceptor'
+import { MemberListComponent } from './member-list/member-list.component'
+import { ListsComponent } from './lists/lists.component'
+import { MessagesComponent } from './messages/messages.component'
+import { appRoutes } from './routes'
 
 @NgModule({
-    declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-    imports: [BrowserModule, HttpClientModule, FormsModule, BrowserAnimationsModule, BsDropdownModule.forRoot()],
+    declarations: [
+        AppComponent,
+        NavComponent,
+        HomeComponent,
+        RegisterComponent,
+        MemberListComponent,
+        ListsComponent,
+        MessagesComponent,
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        BsDropdownModule.forRoot(),
+        RouterModule.forRoot(appRoutes),
+    ],
     providers: [AuthService, ErrorInterCeptorProvider],
     bootstrap: [AppComponent],
 })
